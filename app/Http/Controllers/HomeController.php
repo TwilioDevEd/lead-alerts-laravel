54f98da6b849537fd675b91a040b9d85da8e0c35
Agustin Camino <agustin.camino@gmail.com>
2016-01-07 16:23:26 -0500

    Remove unnecessary test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,17 +9,17 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-  public function index()
-  {
-    $house = [
-      'title' => '555 Sunnybrook Lane',
-      'price' => '$349,999',
-      'description' => 'You and your family will love this charming home. ' .
-      'Featuring granite appliances, stainless steel windows, and ' .
-      'high efficiency dual mud rooms, this joint is loaded to the max. ' .
-      'Motivated sellers have priced for a quick sale, act now!'
-    ];
+    public function index()
+    {
+        $house = [
+            'title' => '555 Sunnybrook Lane',
+            'price' => '$349,999',
+            'description' => 'You and your family will love this charming home. ' .
+            'Featuring granite appliances, stainless steel windows, and ' .
+            'high efficiency dual mud rooms, this joint is loaded to the max. ' .
+            'Motivated sellers have priced for a quick sale, act now!'
+        ];
 
-    return view('home.index', [ 'house' => $house ]);
-  }
+        return view('home.index', [ 'house' => $house ]);
+    }
 }
